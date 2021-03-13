4db61271d0d5fad2312892fee177d4062c44f8da
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2021-03-13 11:16:49 -0700

    added chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 
-export default class Chat extends React.Component {
+export class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  }
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
-    let name = this.props.route.params.name;
-
-    this.props.navigation.setOptions({ title: name });
-
     return (
-      <View>
-        {/* Rest of the UI */}
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     );
-  };
+  }
 }
