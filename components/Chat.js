03cde86ca0f18b65170010cb50725c04886995b2
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2021-03-13 11:55:25 -0700

    typo

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { View, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import React from "react";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
+import { View, Text, Button, Platform, KeyboardAvoidingView } from "react-native";
 
 export default class Chat extends React.Component {
   constructor() {
@@ -10,48 +10,40 @@ export default class Chat extends React.Component {
       messages: [],
     };
   }
-
   componentDidMount() {
     this.setState({
       messages: [
         {
           _id: 1,
-          text: 'Hello developer',
+          text: "Hello developer",
           createdAt: new Date(),
           user: {
             _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
           },
         },
-        {
-          _id: 2,
-          text: 'This is a system message',
-          createdAt: new Date(),
-          system: true,
-        },
       ],
-    })
+    });
   }
+
   onSend(messages = []) {
     this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }));
   }
-
   renderBubble(props) {
     return (
       <Bubble
         {...props}
         wrapperStyle={{
           right: {
-            backgroundColor: '#000'
-          }
+            backgroundColor: "#000",
+          },
         }}
       />
-    )
+    );
   }
-
   render() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
